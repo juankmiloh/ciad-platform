707d880174d77b71e4e983d002d7effecc7b82a2
juankmiloh <juankmiloh@hotmail.com>
2020-11-28 23:48:40 -0500

    Se valida en tarifas si el valor cargado al dataframe es NaN

--- Backend/concret_sources/models/revisor/formulas/FormulaTarifas.py
+++ Backend/concret_sources/models/revisor/formulas/FormulaTarifas.py
@@ -25,44 +25,65 @@ class FormulaTarifas(object):
         # CALCULO TARIFAS ESTRATO 1
         tarifas.loc[(1 - (tarifas[18] * np.minimum((tarifas['ipc_mes_consultado'] / tarifas['ipc_mes_anterior']), (tarifas[21] / tarifas[8])) / tarifas[21])) > 0.6, 'CT_E1'] = (tarifas[21] * (1 - 0.6))
         tarifas.loc[(1 - (tarifas[18] * np.minimum((tarifas['ipc_mes_consultado'] / tarifas['ipc_mes_anterior']), (tarifas[21] / tarifas[8])) / tarifas[21])) <= 0.6, 'CT_E1'] = (tarifas[18] * np.minimum((tarifas['ipc_mes_consultado'] / tarifas['ipc_mes_anterior']), (tarifas[21] / tarifas[8])))
-        tarifas.loc[np.isnan(tarifas['CT_E1']), 'CT_E1'] = 0 # Validamos si es NaN
+        # print('---------------------------------tarifas -> ', tarifas['CT_E1'].isnull().values.any(), '-------------------------')
+
+        if tarifas['CT_E1'].isnull().values.any(): # Validamos si es NaN
+            tarifas['CT_E1'] = 0
 
         # CALCULO TARIFAS ESTRATO 2
         tarifas.loc[(1 - (tarifas[19] * np.minimum((tarifas['ipc_mes_consultado'] / tarifas['ipc_mes_anterior']), (tarifas[21] / tarifas[8])) / tarifas[21])) > 0.5, 'CT_E2'] = (tarifas[21] * (1 - 0.5))
         tarifas.loc[(1 - (tarifas[19] * np.minimum((tarifas['ipc_mes_consultado'] / tarifas['ipc_mes_anterior']), (tarifas[21] / tarifas[8])) / tarifas[21])) <= 0.5, 'CT_E2'] = (tarifas[19] * np.minimum((tarifas['ipc_mes_consultado'] / tarifas['ipc_mes_anterior']), (tarifas[21] / tarifas[8])))
-        tarifas.loc[np.isnan(tarifas['CT_E2']), 'CT_E2'] = 0 # Validamos si es NaN
+        # tarifas.loc[np.isnan(tarifas['CT_E2']), 'CT_E2'] = 0 # Validamos si es NaN
+        if tarifas['CT_E2'].isnull().values.any(): # Validamos si es NaN
+            tarifas['CT_E2'] = 0
 
         # CALCULO TARIFAS ESTRATO 3
         tarifas['CT_E3'] = tarifas[21] * (1 - 0.15)
-        tarifas.loc[np.isnan(tarifas['CT_E3']), 'CT_E3'] = 0 # Validamos si es NaN
+        # tarifas.loc[np.isnan(tarifas['CT_E3']), 'CT_E3'] = 0 # Validamos si es NaN
+        if tarifas['CT_E3'].isnull().values.any(): # Validamos si es NaN
+            tarifas['CT_E3'] = 0
         
         # CALCULO TARIFAS ESTRATO 5
         tarifas['CT_E5'] = tarifas[21] * (1.2)
-        tarifas.loc[np.isnan(tarifas['CT_E5']), 'CT_E5'] = 0 # Validamos si es NaN
+        # tarifas.loc[np.isnan(tarifas['CT_E5']), 'CT_E5'] = 0 # Validamos si es NaN
+        if tarifas['CT_E5'].isnull().values.any(): # Validamos si es NaN
+            tarifas['CT_E5'] = 0
         
         # CALCULO TARIFAS ESTRATO 6
         tarifas['CT_E6'] = tarifas[21] * (1.2)
-        tarifas.loc[np.isnan(tarifas['CT_E6']), 'CT_E6'] = 0 # Validamos si es NaN
+        # tarifas.loc[np.isnan(tarifas['CT_E6']), 'CT_E6'] = 0 # Validamos si es NaN
+        if tarifas['CT_E6'].isnull().values.any(): # Validamos si es NaN
+            tarifas['CT_E6'] = 0
         
         # CALCULO TARIFAS ESTRATO INDUSTRIAL
         tarifas['CT_INDUSTRIAL'] = tarifas[21] * (1.2)
-        tarifas.loc[np.isnan(tarifas['CT_INDUSTRIAL']), 'CT_INDUSTRIAL'] = 0 # Validamos si es NaN
+        # tarifas.loc[np.isnan(tarifas['CT_INDUSTRIAL']), 'CT_INDUSTRIAL'] = 0 # Validamos si es NaN
+        if tarifas['CT_INDUSTRIAL'].isnull().values.any(): # Validamos si es NaN
+            tarifas['CT_INDUSTRIAL'] = 0
         
         # CALCULO TARIFAS ESTRATO COMERCIAL
         tarifas['CT_COMERCIAL'] = tarifas[21] * (1.2)
-        tarifas.loc[np.isnan(tarifas['CT_COMERCIAL']), 'CT_COMERCIAL'] = 0 # Validamos si es NaN
+        # tarifas.loc[np.isnan(tarifas['CT_COMERCIAL']), 'CT_COMERCIAL'] = 0 # Validamos si es NaN
+        if tarifas['CT_COMERCIAL'].isnull().values.any(): # Validamos si es NaN
+            tarifas['CT_COMERCIAL'] = 0
 
         # CALCULO PORCENTAJE SUBSIDIOS ESTRATO 1
         tarifas['CPS_E1'] = (1 - tarifas['CT_E1'] / tarifas[21]) * 100
-        tarifas.loc[np.isnan(tarifas['CPS_E1']), 'CPS_E1'] = 0 # Validamos si es NaN
+        # tarifas.loc[np.isnan(tarifas['CPS_E1']), 'CPS_E1'] = 0 # Validamos si es NaN
+        if tarifas['CPS_E1'].isnull().values.any(): # Validamos si es NaN
+            tarifas['CPS_E1'] = 0
         
         # CALCULO PORCENTAJE SUBSIDIOS ESTRATO 2
         tarifas['CPS_E2'] = (1 - tarifas['CT_E2'] / tarifas[21]) * 100
-        tarifas.loc[np.isnan(tarifas['CPS_E2']), 'CPS_E2'] = 0 # Validamos si es NaN
+        # tarifas.loc[np.isnan(tarifas['CPS_E2']), 'CPS_E2'] = 0 # Validamos si es NaN
+        if tarifas['CPS_E2'].isnull().values.any(): # Validamos si es NaN
+            tarifas['CPS_E2'] = 0
         
         # CALCULO PORCENTAJE SUBSIDIOS ESTRATO 3
         tarifas['CPS_E3'] = (1 - tarifas['CT_E3'] / tarifas[21]) * 100
-        tarifas.loc[np.isnan(tarifas['CPS_E3']), 'CPS_E3'] = 0 # Validamos si es NaN
+        # tarifas.loc[np.isnan(tarifas['CPS_E3']), 'CPS_E3'] = 0 # Validamos si es NaN
+        if tarifas['CPS_E3'].isnull().values.any(): # Validamos si es NaN
+            tarifas['CPS_E3'] = 0
 
         # CALCULO PORCENTAJE SUBSIDIOS ESTRATO 3
         tarifas['CPS_E4'] = 0
@@ -79,7 +100,7 @@ class FormulaTarifas(object):
         # CALCULO PORCENTAJE SUBSIDIOS ESTRATO 3
         tarifas['CPS_COMERCIAL'] = 0
         
-        # print('data TARIFAS -> ', tarifas)
+        print('data TARIFAS -> ', tarifas)
 
         return tarifas
 
